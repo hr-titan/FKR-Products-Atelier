05ea7a3ea5f58da00602a0043f471f95c8805e90
Faycal <rwigemafayk@gmail.com>
2023-12-09 15:33:55 -0800

    getStyles & getRelated

--- server/models/products.js
+++ server/models/products.js
@@ -48,21 +48,29 @@ module.exports = {
           reject(err1);
         }
         if(results1.length === 0) {
-          resolve({});
+          resolve({product_id: id ,results: []});
         }
-        const style = results1.map(result => result.id).join(",");
-        const query2 = `SELECT * FROM photos WHERE photos.style_id IN (${style})`;
+        const styleIDs = results1.map(result => result.id).join(",");
+        const query2 = `SELECT * FROM photos WHERE photos.style_id IN (${styleIDs})`;
         db.query(query2, (err2, results2) => {
           if(err2) {
             reject(err2);
           }
-          const query3 = `SELECT * FROM skus WHERE skus.style_id IN (${style})`;
+          const query3 = `SELECT * FROM skus WHERE skus.style_id IN (${styleIDs})`;
           db.query(query3, (err3, results3) => {
             if(err3) {
               reject(err3);
             }
-            // map through
-            resolve({results1, results2, results3})
+            console.log('::',results3)
+            // map through results1
+            let styles = results1.map((style) => {
+              style['photos'] = results2.filter((photo) => photo.style_id === style.id).map((p) => ({thumbnail_url: p.thumbnail_url, url: p.url}));
+              // style['skus'] = results3.filter((sku) => sku.style_id === style.id);
+              style['skus'] = {}
+              let skus = results3.filter((sku) => sku.style_id === style.id).forEach((s) => style['skus'][s.id] = {size: s.size, quantity: s.quantity});
+              return {...style};
+            });
+            resolve({ product_id: id, results: styles})
         });
       });
     });
@@ -71,12 +79,13 @@ module.exports = {
 
   getRelatedProducts: (id) => {
     return new Promise((resolve, reject) => {
-      const query = ''; // TO DO:
+      const query = `SELECT * FROM relatedProducts WHERE product_id = ${id}`; ; // TO DO:
       db.query(query, (err, results) => {
         if(err) {
           reject(err);
         } else {
-          resolve(results);
+          const related = results.map((res) => res.related_id);
+          resolve(related);
         }
       });
     });
