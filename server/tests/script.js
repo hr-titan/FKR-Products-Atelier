3d8337001b0a1dca1e3eed48520389646d8be085
Faycal <rwigemafayk@gmail.com>
2023-12-11 00:01:52 -0800

    k6 stress tests

--- server/tests/script.js
+++ server/tests/script.js
@@ -1,16 +1,42 @@
 import http from 'k6/http';
+import { check, sleep } from "k6";
+
+export const options = {
+  // define thresholds
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(99)<1000'], // 99% of requests should be below 1s
+  },
+ // define scenarios
+ scenarios: {
+  // arbitrary name of scenario
+  average_load: {
+    executor: 'constant-vus', // use constant VUs executor
+    vus: 1000, // set VUs to this number
+    duration: '10s', // run for 10 seconds
+  },
+ },
+};
 
 export default function () {
-  const res1 = http.get('http://localhost:3000/products');
-  console.log(`Server response time for (GET /products) was ${res1.timings.waiting + res1.timings.receiving} ms`);
+  const random = Math.floor((Math.random() * 10) + 900000);
 
-  const res2 = http.get('http://localhost:3000/products/999999');
-  console.log(`Server response time for (GET /products/:product_id) was ${res2.timings.waiting + res2.timings.receiving} ms`);
+  // const res1 = http.get('http://localhost:3000/products');
+  // http.get(res1);
+  // // console.log(`Server response time for (GET /products) was ${res1.timings.waiting + res1.timings.receiving} ms`);
+  // sleep(1);
 
-  const res3 = http.get('http://localhost:3000/products/435/styles');
-  console.log(`Server response time for (GET /products/:product_id/styles) was ${res3.timings.waiting + res3.timings.receiving} ms`);
+  // var res2 = http.get(`http://localhost:3000/products/${random}`);
+  // http.get(res2);
+  // //console.log(`Server response time for (GET /products/:${random}) was ${res2.timings.waiting + res2.timings.receiving} ms`);
+  // sleep(1);
 
-  const res4 = http.get('http://localhost:3000/products/999999/related');
-  console.log(`Server response time for (GET /products/:product_id/related) was ${res4.timings.waiting + res4.timings.receiving} ms`);
+  // const res3 = http.get(`http://localhost:3000/products/${random}/styles`);
+  // //console.log(`Server response time for (GET /products/:product_id/styles) was ${res3.timings.waiting + res3.timings.receiving} ms`);
+  // sleep(1);
 
+  // const res4 = http.get(`http://localhost:3000/products/${random}/related`);
+  // //console.log(`Server response time for (GET /products/:product_id/related) was ${res4.timings.waiting + res4.timings.receiving} ms`);
+  // sleep(1);
+  
 }
