6eb9f2571f80b639bdba6a32de5a8ad63ecf2ed8
Faycal <rwigemafayk@gmail.com>
2023-12-14 22:17:17 -0800

    minor change - removed a dot

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  user: process.env.DB_USER, .
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 });
 
